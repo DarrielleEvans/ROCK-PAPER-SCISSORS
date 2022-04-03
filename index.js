947e52bea05e54e13164358dbbf23a5939a14009
DarrielleEvans <darriellehenderson@yahoo.com>
2022-04-02 19:34:22 -0500

    created function that will return random choice from computer

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+//create a program that will take user input(rock, paper, or scissors) and play against the computers choice
 
+//this function will randomly return rock, paper, or scissors
 function computerPlay(){
+     choice = ["rock", "papers","scissors"];
+    let randChoice = choice[Math.floor(Math.random() * choice.length)];
+    console.log(choice);
+   // console.log(randChoice);
     
+    return randChoice;
 }
+
+
+
+console.log(computerPlay());
+
