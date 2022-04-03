44cbd6c63b90349c58ffe61878334f3f4014040d
DarrielleEvans <darriellehenderson@yahoo.com>
2022-04-02 21:54:27 -0500

    created function to take in user choice and computer return value

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 //create a program that will take user input(rock, paper, or scissors) and play against the computers choice
 
 //this function will randomly return rock, paper, or scissors
-function computerPlay(){
-     choice = ["rock", "papers","scissors"];
+let computerSelection = function computerPlay(){
+    choice = ["rock", "paper","scissors"];
     let randChoice = choice[Math.floor(Math.random() * choice.length)];
-    console.log(choice);
-   // console.log(randChoice);
-    
+    console.log("Choices: ", choice); 
     return randChoice;
 }
 
+console.log("computer: ",computerSelection());
+
+//prompt user to enter choice
+let input = prompt("Enter: Rock, Paper, or Scissors");
+console.log(input);
+
+//function should take in two parameters(userinput,computerplay's return value)
+//function should compare values to determine winner
+function playGame(player, computer){
+    console.log(player, computer);
+    if (player === computer){
+        console.log("It's a tie");
+    }
+    else if (player === 'rock' && computer === 'papers'){
+        console.log('You Lose! Paper beats rock.');
+    }
+    else if (player === 'scissors' && computer === 'paper'){
+        console.log('You won! Scissors beats paper');
+    }
+    else if (player === 'paper' && computer === 'rock'){
+        console.log("You won! Paper beats rock");
+    }
+
+}
+playGame(input, computerSelection() );
 
 
-console.log(computerPlay());
+
 
