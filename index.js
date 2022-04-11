23bc6f4581ae1febd09b09dc2746dd9c397df0dd
DarrielleEvans <darriellehenderson@yahoo.com>
2022-04-11 00:12:24 -0500

    added function to track score

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 //create a program that will take user input(rock, paper, or scissors) and play against the computers choice
 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+let playerResult = document.getElementById('player');
+let compResult = document.getElementById('comp');
+let showPlayer = document.getElementById('showPlayer');
+let showComp = document.getElementById('showComp');
+let playerTotal = 0;
+let compTotal = 0;
+
+
+
 //this function will randomly return rock, paper, or scissors
 let computerSelection = function computerPlay(){
     choice = ["rock", "paper","scissors"];
@@ -7,45 +19,111 @@ let computerSelection = function computerPlay(){
     //console.log("Choices: ", choice); 
     return randChoice;
 }
+//showComp.textContent = ("computer: ",computerSelection());
 
-//console.log("computer: ",computerSelection());
 
-//prompt user to enter choice
-//let input = prompt("Enter: Rock, Paper, or Scissors");
-//console.log("User: ",input);
-let input;
+rock.addEventListener('click', ()=>{
+    let choice = 'rock';
+    game(choice);
+});
+
+paper.addEventListener('click', ()=>{
+    let choice = 'paper';
+    game(choice);
+});
+
+scissors.addEventListener('click', ()=>{
+    let choice = 'scissors';
+    game(choice);
+});
+
 
 //function should take in two parameters(userinput,computerplay's return value)
 //function should compare values to determine winner
 function playRound(player, computer){
+    let message;
+
     if (player === computer){
         console.log("It's a tie");
+        message = "tie";
+
     }
-    else if ((player === 'rock' && computer === 'papers') || 
+    if ((player === 'rock' && computer === 'papers') || 
         (player === 'scissors' && computer === 'rock') ||
         (player === 'paper' && computer === 'scissors')){
         console.log('You Lose!');
+       // compTotal = compTotal + 1;
+        //compResult.innerText = "Computer: " + compTotal;
+        message =  "loser";
+        
     }
-    else if ((player === 'scissors' && computer === 'paper') ||
-         (player === 'paper' && computer === 'rock') || 
-         (player === 'rock' && computer === 'scissors'))
-         {
+    if ((player === 'scissors' && computer === 'paper') ||
+        (player === 'paper' && computer === 'rock') || 
+        (player === 'rock' && computer === 'scissors'))
+        {
         console.log('You won!');
-         }
-
+        message =  "winner";
+        }
+        return message;
+        /*playerTotal = playerTotal + 1;
+        console.log(playerTotal);
+        
+        }
+        playerResult.innerText = "Player: " + playerTotal; */
+        
 }
+
+//let scoreBoard1;
 
 //function calls the playRound function to determine winner
-function game(){
-   // input = prompt("Enter: Rock, Paper, or Scissors");
-   playRound(input, computerSelection());
-}
+function game(choice){
+   
+   let roundWinner = playRound(choice, computerSelection());
+   console.log("roundWinner: ", roundWinner);
 
-//loops through program 5 times to play five rounds of the game
+   if(roundWinner === "winner"){
+       playerTotal = playerTotal + 1;
+       playerResult.innerText = "Player: " + playerTotal;
+   }
+   else if(roundWinner === "loser"){
+       compTotal = compTotal + 1;
+       compResult.innerText = "Computer: " + compTotal;
+   }
+   showComp.textContent = computerSelection();
+   showPlayer.textContent = choice;
+   
+}
+ 
+ 
+/*console.log(scoreBoard1);
+
+function totals(scoreBoard1){
+    console.log(scoreBoard1);
+ 
+    if (scoreBoard1 === "winner"){
+        playerTotal = playerTotal + 1;
+        console.log("player t", playerTotal);
+
+    }
+    if (scoreBoard1 === "loser"){
+        compTotal = compTotal + 1;
+        console.log("computer t:", compTotal);
+        
+    }
+   
+}*/
+
+
+
+
+
+
+//game();
+/*loops through program 5 times to play five rounds of the game
 for (let i = 0; i < 5; i++){
 game();
 
-}
+}*/
 
 
 
